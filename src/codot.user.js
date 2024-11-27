9108b37bf86557d5f50b85ef1a7be18d8a52e32d
Madjosz <28844868+Madjosz@users.noreply.github.com>
2024-11-27 20:28:21 +0100

    Add example prompts for Java

--- src/codot.user.js
+++ src/codot.user.js
@@ -359,6 +359,13 @@
                 exampleFixes.push('Change classic asserions to `Assert.That`.');
                 exampleFixes.push('Add `[Order]` annotations to test methods.');
                 break;
+            case 'java':
+                exampleFixes.push('Upgrade to JUnit 5 and make test class and test methods package private.');
+                exampleFixes.push('Add `@Order` to test methods and `@TestMethodOrder` to test class.');
+                exampleFixes.push('Add assertion messages showing the input.');
+                exampleFixes.push('Replace usage of `Math.random()` by proper `ThreadLocalRandom` API.');
+                exampleFixes.push('Add `@DisplayName` to test methods.');
+                break;
         }
 
         const examplesDiv = jQuery('#example_fixes_div');
